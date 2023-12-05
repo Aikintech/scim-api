42ec29b67c1781b6a39f47477183a50a14ab925f
Nana Kwesi Ofosu-Aikins <nanaaikinson2020@gmail.com>
2023-12-05 23:39:25 +0000

    Update comment.go

--- pkg/controllers/comment.go
+++ pkg/controllers/comment.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aikintech/scim-api/pkg/utils"
 	"github.com/aikintech/scim-api/pkg/validation"
 	"github.com/gofiber/fiber/v2"
+	"github.com/samber/lo"
 	"gorm.io/gorm"
 )
 
@@ -19,6 +20,7 @@ func NewCommentController() *CommentController {
 	return &CommentController{}
 }
 
+// GetPodcastComments
 func (cmtCtrl *CommentController) GetPodcastComments(c *fiber.Ctx) error {
 	podcastId := c.Params("podcastId", "")
 	podcast := models.Podcast{}
@@ -110,6 +112,7 @@ func (cmtCtrl *CommentController) StorePodcastComment(c *fiber.Ctx) error {
 	}
 
 	trx.Commit()
+
 	avatar, _ := utils.GenerateS3FileURL(*user.Avatar)
 
 	return c.Status(fiber.StatusCreated).JSON(definitions.DataResponse[models.CommentResource]{
@@ -131,10 +134,81 @@ func (cmtCtrl *CommentController) StorePodcastComment(c *fiber.Ctx) error {
 	})
 }
 
-// UpdatePodcastComment - // TODO: Update a podcast comment
+// UpdatePodcastComment - Update a podcast comment
 func (cmtCtrl *CommentController) UpdatePodcastComment(c *fiber.Ctx) error {
+	user := c.Locals(config.USER_CONTEXT_KEY).(*models.User)
+	podcastId := c.Params("podcastId", "")
+	commentId := c.Params("commentId", "")
 
-	return c.SendString("Like podcast")
+	// Parse request
+	request := validation.StorePodcastCommentSchema{}
+	if err := c.BodyParser(&request); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(definitions.MessageResponse{
+			Code:    fiber.StatusBadRequest,
+			Message: err.Error(),
+		})
+	}
+
+	// Validate request
+	if errs := utils.ValidateStruct(request); len(errs) > 0 {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(definitions.ValidationErrsResponse{
+			Code:   fiber.StatusUnprocessableEntity,
+			Errors: errs,
+		})
+	}
+
+	// Find podcast
+	trx := config.DB.Begin()
+	podcast := models.Podcast{}
+	if result := trx.Model(&models.Podcast{}).Preload("Comments").Where("id = ?", podcastId).First(&podcast); result.Error != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(definitions.MessageResponse{
+			Code:    fiber.StatusBadRequest,
+			Message: result.Error.Error(),
+		})
+	}
+
+	// Find comment
+	comment, ok := lo.Find(podcast.Comments, func(item *models.Comment) bool {
+		return item.ID == commentId
+	})
+	if !ok {
+		return c.Status(fiber.StatusNotFound).JSON(definitions.MessageResponse{
+			Code:    fiber.StatusNotFound,
+			Message: "Comment not found",
+		})
+	}
+
+	// Update comment
+	if result := trx.Model(&comment).Update("body", request.Comment); result.Error != nil {
+		trx.Rollback()
+
+		return c.Status(fiber.StatusBadRequest).JSON(definitions.MessageResponse{
+			Code:    fiber.StatusBadRequest,
+			Message: result.Error.Error(),
+		})
+	}
+
+	trx.Commit()
+
+	avatar, _ := utils.GenerateS3FileURL(*user.Avatar)
+
+	return c.Status(fiber.StatusCreated).JSON(definitions.DataResponse[models.CommentResource]{
+		Code: fiber.StatusCreated,
+		Data: models.CommentResource{
+			ID:        comment.ID,
+			Body:      comment.Body,
+			CreatedAt: comment.CreatedAt,
+			User: &models.AuthUserResource{
+				ID:            user.ID,
+				FirstName:     user.FirstName,
+				LastName:      user.LastName,
+				Email:         user.Email,
+				EmailVerified: user.EmailVerifiedAt != nil,
+				Avatar:        &avatar,
+				Channels:      user.Channels,
+			},
+		},
+	})
 }
 
 // DeletePodcastComment - Delete a podcast comment
