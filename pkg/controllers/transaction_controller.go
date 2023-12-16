4c83ed9f0bc76b500ce06de5a87974a098ec65c8
Nana Kwesi Ofosu-Aikins <nanaaikinson2020@gmail.com>
2023-12-16 17:18:04 +0000

    Files update

--- pkg/controllers/transaction_controller.go
+++ pkg/controllers/transaction_controller.go
@@ -1,5 +1,11 @@
 package controllers
 
+import (
+	"github.com/aikintech/scim-api/pkg/definitions"
+	"github.com/aikintech/scim-api/pkg/validation"
+	"github.com/gofiber/fiber/v2"
+)
+
 type TransactionController struct{}
 
 func NewTransactionController() *TransactionController {
@@ -7,3 +13,20 @@ func NewTransactionController() *TransactionController {
 }
 
 // Client handlers
+func (t *TransactionController) Transact(c *fiber.Ctx) error {
+	// Parse request
+	request := new(definitions.DonateRequest)
+	if err := c.BodyParser(request); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(definitions.MessageResponse{
+			Message: err.Error(),
+		})
+	}
+
+	// Validate request
+	if errs := validation.ValidateStruct(request); len(errs) > 0 {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(definitions.ValidationErrsResponse{
+			Errors: errs,
+		})
+	}
+	return c.SendString("Transact")
+}
